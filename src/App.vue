e4ced984306bb5f1ea0201054ee0de850ee660b8
Pola-ris <54174343+Pola-ris@users.noreply.github.com>
2021-06-20 17:12:24 +0800

    Jun20

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
+    <router-view></router-view>
 
+    <maintabbar/>
   </div>
 </template>
 
 
 <script>
-
+    import maintabbar from 'components/content/maintabbar';
 
 export default {
   name: 'app',
   components:{
-
+    maintabbar
   }
 }
 </script>
 
 <style>
-@import "./assets/css/base.css";
+@import "assets/css/base.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/content/tabControl/tabControl.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item, index) in titles"
+      class="tab-bar-control"
+         :class="{active: index === currentIndex}"
+         @click="itemClick(index)">
+      <span>{{item}}</span>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabControl",
+  props:{
+    titles:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data(){
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+      this.$emit('tabClick', index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
